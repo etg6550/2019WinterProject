d5f35cb722d79086f31eda3ce5673873284fd57f
etg6550 <etg9307@naver.com>
2019-02-09 16:09:49 +0900

    until CH6

---
+++ Day16/Spring-Boot-Community-Rest/data-rest/src/main/java/com/community/rest/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.community.rest.repository;
+
+
+import com.community.rest.domain.User;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+
+@RepositoryRestResource/*(excerptProjection = UserOnlyContainName.class)*/
+public interface UserRepository extends JpaRepository<User, Long> {
+
+}
